f629785a9d70839cb44133f9a61ac87f24aa4d86
Steven Van Acker <steven@singularity.be>
2016-01-28 14:49:44 +0100

    scan for JS, CSS, flash and java applets

--- crawler/afterclickshandlers.py
+++ crawler/afterclickshandlers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import logging, sys, json
+import logging, sys, json, pprint
 from time import sleep
 from urllib.parse import urljoin
 
@@ -62,6 +62,104 @@ class LoginPageChecker(BaseAfterClicksHandler): #{{{
             "pre_clicks": [x.toDict() if x != None else None for x in self.preclicks],
         }
 
+    def getResourceData(self, url, page):
+        logging.debug("Retrieving resources from {}".format(url))
+        outdata = {}
+        
+        # Find script resources
+        elements = page.mainFrame().findAllElements('script')
+        pairs = [(s.attribute("src", "").strip(), s.attribute("type", "").strip(), s.attribute("integrity", "")) for s in elements]
+        urls = {}
+        for (s, t, sri) in pairs:
+            if s != "":
+                nt = None
+                if t == "" or "javascript" in t.lower():
+                    nt = "javascript"
+                if"vbscript" in t.lower():
+                    nt = "vbscript"
+                if nt != None:
+                    ns = urljoin(url, s)
+                    urls[ns] = { "type": nt, "sri": sri!=""}
+        outdata["script"] = urls
+
+        # Find css resources
+        elements = page.mainFrame().findAllElements('link[rel="stylesheet"]')
+        pairs = [(s.attribute("href", "").strip(), s.attribute("integrity", "")) for s in elements]
+        urls = {}
+        for (s, sri) in pairs:
+            if s != "":
+                ns = urljoin(url, s)
+                urls[ns] = { "sri": sri!=""}
+        outdata["css"] = urls
+
+        # Find object resources
+        elements = page.mainFrame().findAllElements('object')
+        pairs = [(s.attribute("data", "").strip(), s.attribute("type", "").strip(), s.attribute("typemustmatch", "false"), s.attribute("classid", "").strip(), s.attribute("integrity", "")) for s in elements]
+        urls = {}
+        for (d, t, tmm, classid, sri) in pairs:
+            logging.debug("Detected Object: {} {} {} {} {}".format(d, t, tmm, classid, sri))
+            nsri = (sri!="")
+            ntmm = (tmm.lower()=="true")
+            if d != "":
+                nt = ""
+                if classid.lower() == "clsid:d27cdb6e-ae6d-11cf-96b8-444553540000":
+                    nt = "flash"
+                if "flash" in t.lower():
+                    nt = "flash"
+
+                if classid.lower().startswith("clsid:CAFEEFAC-00".lower()) and classid.lower().endswith("13-0001-FFFF-ABCDEFFEDCBA".lower()):
+                    nt = "java"
+                if "java" in t.lower():
+                    nt = "java"
+                    
+                if nt != "":
+                    nd = urljoin(url, d)
+                    urls[nd] = {"sri": nsri, "typemustmatch": ntmm, "type": nt}
+
+        outdata["object"] = urls
+
+        # Find embed resources
+        elements = page.mainFrame().findAllElements('embed')
+        pairs = [(s.attribute("src", "").strip(), s.attribute("type", "").strip(), s.attribute("integrity", "")) for s in elements]
+        urls = {}
+        for (s, t, sri) in pairs:
+            logging.debug("Detected Embed: {} {} {}".format(s,t,sri))
+            nsri = (sri!="")
+            if s != "":
+                nt = ""
+                if "flash" in t.lower():
+                    nt = "flash"
+                if t == "" and s.lower().endswith(".swf"):
+                    nt = "flash"
+
+                if "java" in t.lower():
+                    nt = "java"
+                if t == "" and s.lower().endswith((".jar", ".class", ".jnlp")):
+                    nt = "java"
+                    
+                if nt != "":
+                    nd = urljoin(url, s)
+                    urls[nd] = {"sri": nsri, "type": nt}
+
+        outdata["embed"] = urls
+
+        # Find applet resources
+        elements = page.mainFrame().findAllElements('applet')
+        pairs = [(s.attribute("archive", "").strip(), s.attribute("code", "").strip(), s.attribute("codebase", "").strip(), s.attribute("object", "").strip(), s.attribute("src", "").strip(), s.attribute("integrity", "")) for s in elements]
+        urls = []
+        for (a, c1, c2, o, s, sri) in pairs:
+            logging.debug("Detected Applet: {} {} {} {} {} {}".format(a, c1, c2, o, s, sri))
+            nsri = (sri!="")
+            [a, c1, c2, o, s] = [urljoin(url, x) if x != "" else "" for x in [a, c1, c2, o, s]]
+            if (a, c1, c2, o, s) != ("", "", "", "", ""):
+                urls.append((a, c1, c2, o, s, sri))
+
+        outdata["applet"] = urls
+
+
+        logging.info(pprint.pformat(outdata))
+
+
     def handle(self, data, errorcode):
         self.initclick = data["element_to_click"]
         self.preclicks = data["pre_clicks"]
@@ -78,6 +176,8 @@ class LoginPageChecker(BaseAfterClicksHandler): #{{{
         logging.debug("Logging something so that jAEK doesn't crap out...")
         if len(passwordfields) > 0:
             data["self"].screenshot("screenshot.png")
+
+        self.getResourceData(self.url, data["self"])
         self.resultFlag = True
 #}}}
 
